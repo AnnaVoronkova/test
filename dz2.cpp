d4d21a776a761f54463917eb4caec1e85827e80e
AnnaVoronkova <63228705+AnnaVoronkova@users.noreply.github.com>
2020-04-06 13:58:24 +0300

    Add files via upload

---
+++ dz2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct student {  //структура с параметрами ученика
+    string last_name;
+    string name;
+    int math;
+    int phis;
+    int info;
+};
+bool cmp(student a, student b) { //компаратор для sort
+    return ((b.math + b.phis + b.info)/3) < ((a.math + a.phis + a.info)/3);
+
+}
+int main() {
+    int n;
+    cin >> n;
+    vector <student> a(n);
+    for (int i = 0; i < n; i++)
+    cin >> a[i].last_name >> a[i].name >> a[i].math >> a[i].phis >> a[i].info;
+    stable_sort(a.begin(), a.end(), cmp);
+    for (auto now : a)
+    cout << now.last_name << " " << now.name << endl;
+    return 0;
+}
+
